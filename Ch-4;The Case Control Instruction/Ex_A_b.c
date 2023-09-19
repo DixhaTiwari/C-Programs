4b427e08451113f0b3fc8426213b40c0fc877d0f
Dixha Tiwari <129823357+DixhaTiwari@users.noreply.github.com>
2023-09-19 15:47:24 +0530

    Add files via upload

---
+++ Ch-4;The Case Control Instruction/Ex_A_b.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int c=3;
+ switch(c)
+{
+  case '3':
+    printf("Diamonds\n");
+    break;
+    case 3:
+    printf("Spade\n");
+    break;
+    default:
+    printf("Hearts\n");
+}
+
+    return 0;
+}
---
+++ Ch-4;The Case Control Instruction/Ex_A_c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i=3;
+ switch(i)
+{
+  case 0:
+    printf("Customers\n");
+    case 1+0:
+    printf("Markets\n");
+    case 4/2:
+     printf("Investors\n");
+    case 8%5:
+    printf("Employees\n");
+
+}
+
+    return 0;
+}
